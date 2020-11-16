5534e5e81139855fb4fc755731c4ccb9c8512617
Adetayo Omotomiwa <adetayoomotomiwa99@gmail.com>
2020-11-15 22:03:24 -0800

    livescore update

---
+++ precache-manifest.7be36bf16da9228b235f81a5069957fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676cb6ee9b7f98284bae64196d96e2f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "382a4080f8c0ce99e88d",
+    "url": "/static/css/main.1a51c193.chunk.css"
+  },
+  {
+    "revision": "50174c7bfebb09da0c6d",
+    "url": "/static/js/2.3cc0dab1.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.3cc0dab1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382a4080f8c0ce99e88d",
+    "url": "/static/js/main.9040d9b1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
